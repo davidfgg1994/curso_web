aacf95a7337c26a498da72858b10eb5d5ab7ae4b
David <CursosTardes@tic.loc>
2019-05-28 21:27:25 +0200

    28 de MAYO

---
+++ 08_booleans.js
@@ -0,0 +1,18 @@
+let x= 34
+// X= -5  
+console.log(x >=0)
+if (x >= 0) {
+
+    console.log ('Positivo')
+    
+} else {
+    console.log ('negativo')
+}
+
+if (x) {
+
+    console.log ('Positivo')
+    
+} else {
+    console.log ('negativo')
+}
