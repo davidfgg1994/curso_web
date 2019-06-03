9287c2d4ab4e69a29d1eeb0f45f0fb1916be6138
David <CursosTardes@tic.loc>
2019-06-03 21:24:33 +0200

    3 junio

---
+++ 09_intro_js/spec/11_pares_try.spec.js
@@ -0,0 +1,25 @@
+describe('pruebas de números pares', () => {
+
+    it( 'should be "impar" if n = 1',  () => {
+        x=1
+       expect  (esPar(x)).toContain()
+    })
+    
+    it( 'should be "par" if n = 2',  () => {
+        x=2
+         expect (esPar(x)). toEqua1 ()
+    })
+      
+        x = 345
+        x = 98
+        x = 0
+        x = -4
+        x = -5
+        x = 4.3
+        x = -56.7
+        x = 'pepe'
+        x = '56'
+        mostrar(x)
+        
+
+})
